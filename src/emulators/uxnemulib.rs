125004d46b1687b6555e4871a676015ef71ce553
John Sharp <johncolinsharp@gmail.com>
2022-06-25 15:19:00 +0100

    triggering screen refresh event every 1/60 second

--- src/emulators/uxnemulib.rs
+++ src/emulators/uxnemulib.rs
@@ -6,11 +6,14 @@ use std::io;
 use std::io::BufReader;
 use std::io::Read;
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
 use speedy2d::Window;
-use speedy2d::window::{WindowHandler, WindowHelper, WindowStartupInfo};
+use speedy2d::window::{WindowHandler, WindowHelper, WindowStartupInfo, WindowCreationOptions, WindowSize};
 use speedy2d::Graphics2D;
 use speedy2d::color::Color;
+use speedy2d::dimen::Vector2;
 
 use crate::ops::OpObjectFactory;
 use crate::emulators::devices::console::Console;
@@ -46,15 +49,19 @@ impl fmt::Display for RomReadError {
 
 impl Error for RomReadError {}
 
+enum UxnEvent {
+    ScreenRefresh,
+}
+
 struct MyWindowHandler<J: instruction::InstructionFactory, K: Write, L: Write, M: Write> {
     uxn: uxn::UxnImpl<J>,
     console_device: Console<L, M>,
     debug_writer: K,
 }
 
-impl<J: instruction::InstructionFactory, K: Write, L: Write, M: Write>  WindowHandler for MyWindowHandler<J, K, L, M>
+impl<J: instruction::InstructionFactory, K: Write, L: Write, M: Write>  WindowHandler<UxnEvent> for MyWindowHandler<J, K, L, M>
 {
-    fn on_draw(&mut self, _helper: &mut WindowHelper, graphics: &mut Graphics2D)
+    fn on_draw(&mut self, _helper: &mut WindowHelper<UxnEvent>, graphics: &mut Graphics2D)
     {
         // Draw things here using `graphics`
         graphics.clear_screen(Color::from_rgb(0.8, 0.9, 1.0));
@@ -63,7 +70,7 @@ impl<J: instruction::InstructionFactory, K: Write, L: Write, M: Write>  WindowHa
 //        helper.request_redraw();
     }
 
-    fn on_start(&mut self, _helper: &mut WindowHelper, _info: WindowStartupInfo) {
+    fn on_start(&mut self, _helper: &mut WindowHelper<UxnEvent>, _info: WindowStartupInfo) {
         // TODO run uxn from init vector
         let mut device_list: HashMap::<u8, DeviceEntry<&mut K>> = HashMap::new();
         device_list.insert(0x0, DeviceEntry::SystemPlaceHolder(&mut self.debug_writer));
@@ -73,6 +80,18 @@ impl<J: instruction::InstructionFactory, K: Write, L: Write, M: Write>  WindowHa
         self.uxn.run(uxn::INIT_VECTOR, device_list);
         // start thread that sleeps for 1/60 second and then triggers an event to trigger
         // the screen draw vector
+    }
+
+    fn on_user_event(
+        &mut self,
+        helper: &mut WindowHelper<UxnEvent>,
+        user_event: UxnEvent
+    ) {
+        match user_event {
+            UxnEvent::ScreenRefresh => {
+                println!("should be refreshing screen here");
+            },
+        }
     }
 }
 
@@ -94,7 +113,16 @@ pub fn run<J: Write + 'static>(cli_config: Cli, other_config: Config<J>) -> Resu
     let console_device = Console::new(io::stdout(), io::stderr());
 
 
-    let window = Window::new_centered("Title", (512, 320)).unwrap();
+    let window = Window::<UxnEvent>::new_with_user_events("Title", WindowCreationOptions::new_windowed(WindowSize::PhysicalPixels(Vector2::new(512, 320)), None)).unwrap();
+
+    let window_refresh_event_sender = window.create_user_event_sender();
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_millis(17));
+            window_refresh_event_sender.send_event(UxnEvent::ScreenRefresh).unwrap();
+        }
+    });
+
     window.run_loop(MyWindowHandler{
         uxn, console_device, debug_writer: other_config.stderr_writer,
     });
